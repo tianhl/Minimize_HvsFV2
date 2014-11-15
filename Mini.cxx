324ae4148b99f730ac42f52d4f53c66078bb1cba
tianhl <haolai.tian@gmail.com>
2014-11-15 16:28:35 +0800

    function

--- Mini.cxx
+++ Mini.cxx
@@ -12,10 +12,77 @@
 #include "TCanvas.h"
 #include "TMultiGraph.h"
 
+//========================================================================
 std::vector<int> Temperature;
 std::vector<double> Magneticfield;
 std::vector<double> Frequency;
 std::map<int, std::vector<int> > Group;
+
+//========================================================================
+double H(const double *xx)
+{
+  const double M     = xx[0];
+  const double K1    = xx[1];
+  const double K2    = xx[2];
+  const double Phi   = xx[3];
+  const double Alpha = xx[4];
+
+  const double Eq1   = -1/(M*sin(Phi-Alpha));
+  const double Eq2   = (4*M_PI*M*M-2*K1-4*K2)*cos(Phi)*sin(Phi);
+  const double Eq3   = 4*K2*cos(Phi)*sin(Phi)*sin(Phi)*sin(Phi);
+
+  return Eq1*(Eq2+Eq3);
+}
+
+double H1(const double *xx)
+{
+  const double M     = xx[0];
+  const double K1    = xx[1];
+  const double K2    = xx[2];
+  const double Phi   = xx[3];
+  const double Alpha = xx[4];
+
+  const double Eq1   = -1/(M*sin(Phi-Alpha));
+  const double Eq2   = (4*M_PI*M*M-2*K1-4*K2)*cos(Phi)*sin(Phi);
+  const double Eq3   = 4*K2*cos(Phi)*sin(Phi)*sin(Phi)*sin(Phi);
+  const double Eq4   = M*cos(Alpha-Phi);
+  const double Eq5   = (4*M_PI*M*M-2*K1-4*K2)*cos(2*Phi);
+  const double Eq6   = 4*K2*(3*sin(Phi)*sin(Phi)*cos(Phi)*cos(Phi)-sin(Phi)*sin(Phi)*sin(Phi)*sin(Phi));
+
+  return Eq1*(Eq2+Eq3)*Eq4+Eq5+Eq6;
+}
+
+double H2(const double *xx)
+{
+  const double M     = xx[0];
+  const double K1    = xx[1];
+  const double K2    = xx[2];
+  const double Phi   = xx[3];
+  const double Alpha = xx[4];
+
+  const double Eq1   = -1/(M*sin(Phi-Alpha));
+  const double Eq2   = (4*M_PI*M*M-2*K1-4*K2)*cos(Phi)*sin(Phi);
+  const double Eq3   = 4*K2*cos(Phi)*sin(Phi)*sin(Phi)*sin(Phi);
+  const double Eq4   = M*cos(Alpha-Phi);
+  const double Eq5   = (4*M_PI*M*M-2*K1-4*K2)*sin(Phi)*sin(Phi);
+  const double Eq6   = 4*K2*sin(Phi)*sin(Phi)*sin(Phi)*sin(Phi);
+
+
+  return Eq1*(Eq2+Eq3)*Eq4-Eq5-Eq6;
+}
+
+double F(const double *xx)
+{
+  const double M     = xx[0];
+  return 1.76*1e7/(2*M_PI*M)*sqrt(H1(xx)*H2(xx));	
+}
+
+void HvsF(const double *xx, double *h, double *f){
+  *h = H(xx);
+  *f = F(xx);
+}
+
+//========================================================================
 
 void readfile(std::string& datafilename){
 	std::ifstream datafile(datafilename.c_str());
@@ -109,6 +176,8 @@ void plotdata(){
 	cav->Print("FunGraph.eps");
 
 }
+
+//========================================================================
 
 int main(int argc, char *argv[]){
 	if(argc != 2){
